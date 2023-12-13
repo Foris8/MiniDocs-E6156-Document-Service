aab82df1d86b99412a72c3e86b68c62a612c1f7c
Foris8 <foristang@gmail.com>
2023-12-12 21:15:55 -0600

    add SSN notification

--- app/main.py
+++ app/main.py
@@ -3,12 +3,23 @@ import boto3
 from botocore.exceptions import ClientError
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Union
+from typing import List, Union
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 region = os.getenv('DB_REGION_NAME')
 access_key_id = os.getenv('DB_ACCESS_KEY_ID')
@@ -22,14 +33,30 @@ dynamodb = boto3.resource(
     aws_secret_access_key=secret_access_key
 )
 
+# SNS call
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIA5G5LLVRWR7HS3YVA'
+os.environ['AWS_SECRET_ACCESS_KEY'] = 'tYGIg9Sgtu4w441XaGlnQoZ1HpLpdNPXQkwq7yRB'
+# Create an SNS client
+sns = boto3.client('sns', region_name='us-east-1')
+
+
 
 # Reference to your DynamoDB table
 table = dynamodb.Table('document')
 
+
 class Document(BaseModel):
     document_id: str
     create_time: str
+    owner: str
+    document_name: str
     content: str
+    viewers: list[str]
+
+
+class UpdateDocument(BaseModel):
+    content: Union[str, None] = None
+    viewers: Union[List[str], None] = None
 
 @app.post("/documents/")
 def create_document(document: Document):
@@ -38,12 +65,16 @@ def create_document(document: Document):
             Item={
                 'documentId': document.document_id,
                 'CreateTime': document.create_time,
-                'content': document.content
+                'Owner': document.owner,
+                'DocumentName': document.document_name,
+                'Content': document.content,
+                'Viewers': document.viewers
             }
         )
         return response
     except ClientError as e:
         raise HTTPException(status_code=500, detail=str(e))
+
 
 @app.get("/documents/{document_id}/{create_time}")
 def read_document(document_id: str, create_time: str):
@@ -58,24 +89,40 @@ def read_document(document_id: str, create_time: str):
     except ClientError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.put("/documents/{document_id}/{create_time}")
-def update_document(document_id: str, create_time: str, content: Union[str, None] = None):
+def update_document(document_id: str, create_time: str, update_data: UpdateDocument):
     try:
+        update_expression = "set"
+        expression_attribute_values = {}
+
+        if update_data.content is not None:
+            update_expression += " Content = :c,"
+            expression_attribute_values[':c'] = update_data.content
+
+        if update_data.viewers is not None:
+            update_expression += " Viewers = :v"
+            expression_attribute_values[':v'] = update_data.viewers
+
         response = table.update_item(
             Key={
                 'documentId': document_id,
                 'CreateTime': create_time
             },
-            UpdateExpression="set content = :c",
-            ExpressionAttributeValues={
-                ':c': content
-            },
+            UpdateExpression=update_expression.rstrip(','),
+            ExpressionAttributeValues=expression_attribute_values,
             ReturnValues="UPDATED_NEW"
+        )
+        
+        # Publish a message to the topic
+        SNS_response = sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:908208090221:MyTestTopic',
+            Message='minidocse6156@gmail.com,This is a message from python!'
         )
         return response
     except ClientError as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
 
 @app.delete("/documents/{document_id}/{create_time}")
 def delete_document(document_id: str, create_time: str):
@@ -89,4 +136,20 @@ def delete_document(document_id: str, create_time: str):
         return response
     except ClientError as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
+@app.get("/documents/")
+def read_all_documents(last_evaluated_key: Union[str, None] = None):
+    try:
+        scan_kwargs = {}
+        if last_evaluated_key:
+            scan_kwargs['ExclusiveStartKey'] = {
+                'documentId': last_evaluated_key}
+
+        response = table.scan(**scan_kwargs)
+        items = response.get('Items', [])
+        last_evaluated_key = response.get('LastEvaluatedKey')
+
+        return {"items": items, "last_evaluated_key": last_evaluated_key}
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
